acc28f108e271602131b983bbdfd69ff8d6bb370
JoshThePilot <fightjojo@gmail.com>
2025-07-02 11:11:12 +0800

    0702

---
+++ 0702/q2seqsearch.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class q2seqsearch {
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int key = scanner.nextInt();
+
+        boolean found = false;
+        int ops = 0;
+
+        for (int i = 0; i < n; i++) {
+            ops++;
+            if (arr[i] == key) {
+                found = true;
+                break;
+            }
+        }
+
+        System.out.println(found ? "Yes" : "No");
+        System.out.println(ops);
+
+        scanner.close();
+    }
+}
---
+++ 0702/q7permutation.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class q7permutation {
+    static int ops = 0;
+
+    public static void permute(int[] arr, int start, int n) {
+        if (start == n) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i]);
+                if (i < n - 1) System.out.print(" ");
+            }
+            System.out.println();
+            ops++;
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            swap(arr, start, i);
+            permute(arr, start + 1, n);
+            swap(arr, start, i);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+
+        permute(arr, 0, n);
+        System.out.println(ops);
+
+        scanner.close();
+    }
+}
